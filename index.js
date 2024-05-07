7bb89d3f3fc599a22a8c81ae4b639a9257697fb1
elop123 <142408289+elop123@users.noreply.github.com>
2024-05-07 09:19:38 +0200

    file

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import http from 'http';
 
- const port = 4000;
+ const port = 3000;
 
 http.createServer((request, response) => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Webserver is running now on http://localhost:${port}`);
     response.writeHead(200, {'Content-Type':'text/plain'});
     response.write('Hello World');
     response.end();
