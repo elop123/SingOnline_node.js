6ccccd36b62d97f685946729ca8114eb59d4067e
elop123 <142408289+elop123@users.noreply.github.com>
2024-05-14 08:52:14 +0200

    index

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-import http from 'http';
+// import http from 'http';
 
- const port = 3000;
+//  const port = 3000;
 
-http.createServer((request, response) => {
+// http.createServer((request, response) => {
+//     console.log(`Webserver is running now on http://localhost:${port}`);
+//     response.writeHead(200, {'Content-Type':'text/plain'});
+//     response.write('Hello World');
+//     response.end();
+// }).listen(port)
+
+import express from 'express'
+
+const app = express()
+const port = 3000
+
+app.get('/',(req, res)=>{
+    console.log(req.query);
+  res.send('Forside')
+})
+
+app.post('/', (req, res)=>{
+    res.send('Endpoint til POST')
+})
+
+app.listen(3000, ()=>{
     console.log(`Webserver is running now on http://localhost:${port}`);
-    response.writeHead(200, {'Content-Type':'text/plain'});
-    response.write('Hello World');
-    response.end();
-}).listen(port)
+})
